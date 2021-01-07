9bae9c97a384315e2e3290836f6c02ba6b4f96a7
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-01-07 13:09:23 -0800

    Move handling recordings to its own file

---
+++ brain/src/main/kotlin/plp/brain/HandleRecordings.kt
@@ -0,0 +1,37 @@
+package plp.brain
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import plp.data.Database
+import plp.proto.AudioRecordingOuterClass
+import java.nio.file.Path
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.div
+import kotlin.io.path.writeBytes
+
+private val logger = KotlinLogging.logger {}
+
+@ExperimentalPathApi
+fun handleRecordings(audioDirectory: Path, database: Database, recordings: Flow<AudioRecordingOuterClass.Recording>) =
+    runBlocking {
+        val queries = database.audioQueries
+
+        recordings.collect { recording: AudioRecordingOuterClass.Recording ->
+            logger.info { "received recording from ${recording.timestamp}" }
+
+            // Write audio to file
+            val filename = "${recording.timestamp}.wav"
+            val filePath = audioDirectory / filename
+            filePath.writeBytes(recording.audio.toByteArray())
+            logger.debug { "wrote ${recording.audio.size()} bytes to $filePath" }
+
+            // Save recording to database
+            queries.insert(
+                filename,
+                recording.timestamp.toLong(),
+                RECORDING_SEGMENT_DURATION_SECONDS.toDouble()
+            ) // FIXME: use computed recording duration
+        }
+    }
--- brain/src/main/kotlin/plp/brain/RecordingClient.kt
+++ brain/src/main/kotlin/plp/brain/RecordingClient.kt
@@ -3,16 +3,11 @@ package plp.brain
 import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
-import plp.data.Database
 import plp.proto.AudioRecordingGrpcKt
 import plp.proto.AudioRecordingOuterClass
 import java.nio.file.Path
 import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.div
-import kotlin.io.path.writeBytes
 
 const val EAR_HOST = "localhost"
 const val DEFAULT_PORT = 50058
@@ -46,26 +41,3 @@ class MutualAuthRecordingClient(private val root: Path, private val cert: Path, 
         return stub.streamRecordings(recordingSpec)
     }
 }
-
-@ExperimentalPathApi
-fun handleRecordings(audioDirectory: Path, database: Database, recordings: Flow<AudioRecordingOuterClass.Recording>) =
-    runBlocking {
-        val queries = database.audioQueries
-
-        recordings.collect { recording: AudioRecordingOuterClass.Recording ->
-            logger.info { "received recording from ${recording.timestamp}" }
-
-            // Write audio to file
-            val filename = "${recording.timestamp}.wav"
-            val filePath = audioDirectory / filename
-            filePath.writeBytes(recording.audio.toByteArray())
-            logger.debug { "wrote ${recording.audio.size()} bytes to $filePath" }
-
-            // Save recording to database
-            queries.insert(
-                filename,
-                recording.timestamp.toLong(),
-                RECORDING_SEGMENT_DURATION_SECONDS.toDouble()
-            ) // FIXME: use computed recording duration
-        }
-    }
