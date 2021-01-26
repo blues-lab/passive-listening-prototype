5c03d3b988acf0c9becbbe23dab64f689c589374
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-01-25 22:53:40 -0800

    Deprecate unused logging helper

--- common/src/main/kotlin/plp/common/Logging.kt
+++ common/src/main/kotlin/plp/common/Logging.kt
@@ -1,10 +1,11 @@
 package plp.common
 
+@Deprecated("this project currently doesn't use SLF4J")
 fun configureLogging() {
-    System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace")
-
-    val path: String =
-        {}.javaClass.getResource("logging.properties").file // NOTE: will look for src/main/resources/plp/common/logging.properties
-    val loggingConfig = System.getProperty("java.util.logging.config.file", path)
-    System.setProperty("java.util.logging.config.file", loggingConfig)
+    // System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "trace")
+    //
+    // val path: String =
+    //     {}.javaClass.getResource("logging.properties").file // NOTE: will look for src/main/resources/plp/common/logging.properties
+    // val loggingConfig = System.getProperty("java.util.logging.config.file", path)
+    // System.setProperty("java.util.logging.config.file", loggingConfig)
 }
