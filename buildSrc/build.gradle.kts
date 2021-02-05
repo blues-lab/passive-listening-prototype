4eab729a712a5a070ce1a080076a8b46d399ee2b
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-02-04 16:21:55 -0800

    Upgrade to Kotlin 1.4.30

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.4.30"
 }
 
 repositories {
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.36.0")
