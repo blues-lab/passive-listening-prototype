9bca0f4f911716ccd7864c32a809e9c9ef8ab6d8
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-02-19 17:06:49 -0800

    Only determine the OS once

--- hub/src/main/kotlin/plp/hub/recording/MacRecorder.kt
+++ hub/src/main/kotlin/plp/hub/recording/MacRecorder.kt
@@ -7,30 +7,34 @@ import plp.logging.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-val RecordMac = RecordOnce { durationSeconds, path ->
-    logger.info("recording $durationSeconds seconds using ffmpeg to $path")
+val RecordMac: RecordOnce = run {
     val ffmpegEncodingFormat = when (getOS()) {
         OS.LINUX -> "alsa"
         OS.MAC -> "avfoundation"
         else -> "unknown"
     }
+    logger.debug { "setting up FfmpegRecorder using $ffmpegEncodingFormat" }
 
-    runCommandAndGetOutput(
-        listOf(
-            "ffmpeg",
-            "-f",
-            ffmpegEncodingFormat,
-            "-i",
-            ":0",
-            "-t",
-            durationSeconds.toString(),
-            "-ac",
-            "1",
-            "-ar",
-            "16k",
-            path.toString()
+    RecordOnce { durationSeconds, path ->
+        logger.info("recording $durationSeconds seconds using ffmpeg to $path")
+
+        runCommandAndGetOutput(
+            listOf(
+                "ffmpeg",
+                "-f",
+                ffmpegEncodingFormat,
+                "-i",
+                ":0",
+                "-t",
+                durationSeconds.toString(),
+                "-ac",
+                "1",
+                "-ar",
+                "16k",
+                path.toString()
+            )
         )
-    )
 
-    logger.debug("recording finished $path")
+        logger.debug("recording finished $path")
+    }
 }
