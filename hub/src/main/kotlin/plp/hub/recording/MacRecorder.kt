61cca55d31c6b0f5f0ac49ac34b9398e65f87cde
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-02-19 17:03:19 -0800

    Move utility code to common package

--- hub/src/main/kotlin/plp/hub/recording/MacRecorder.kt
+++ hub/src/main/kotlin/plp/hub/recording/MacRecorder.kt
@@ -1,36 +1,11 @@
 package plp.hub.recording
 
+import plp.common.OS
+import plp.common.getOS
 import plp.common.runCommandAndGetOutput
 import plp.logging.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
-
-enum class OS {
-    UNKNOWN, WINDOWS, LINUX, MAC, SOLARIS
-}
-
-/**
- * Get the current system's OS
- * via https://stackoverflow.com/a/31547504
- */
-fun getOS(): OS {
-    val os = System.getProperty("os.name").toLowerCase()
-    return when {
-        os.contains("win") -> {
-            OS.WINDOWS
-        }
-        os.contains("nix") || os.contains("nux") || os.contains("aix") -> {
-            OS.LINUX
-        }
-        os.contains("mac") -> {
-            OS.MAC
-        }
-        os.contains("sunos") -> {
-            OS.SOLARIS
-        }
-        else -> OS.UNKNOWN
-    }
-}
 
 val RecordMac = RecordOnce { durationSeconds, path ->
     logger.info("recording $durationSeconds seconds using ffmpeg to $path")
