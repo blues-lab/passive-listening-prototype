c028aa583c6f62ea56518b68d34f9c074f2ede78
Nathan Malkin <nmalkin@cs.berkeley.edu>
2021-06-07 08:39:04 -0700

    Update coroutine invocation with new usage

--- classify_client/src/main/kotlin/plp/classify/ClassifyClientMain.kt
+++ classify_client/src/main/kotlin/plp/classify/ClassifyClientMain.kt
@@ -10,29 +10,31 @@ import kotlin.io.path.ExperimentalPathApi
 
 @ExperimentalCoroutinesApi
 @ExperimentalPathApi
-fun main(args: Array<String>) = runBlocking {
-    val parser = ArgParser("ClassifyClientMain")
-    val text by parser.option(
-        ArgType.String,
-        shortName = "t",
-        description = "the text you want to classify. If not specified, will read it from STDIN continuously."
-    )
-    val root by parser.option(ArgType.String, description = "path to root certificate chain, if using TLS")
-    val key by parser.option(ArgType.String, description = "path to secret key file, if using mutual TLS")
-    val cert by parser.option(ArgType.String, description = "path to public certificate, if using mutual TLS")
-    parser.parse(args)
+fun main(args: Array<String>) {
+    runBlocking {
+        val parser = ArgParser("ClassifyClientMain")
+        val text by parser.option(
+            ArgType.String,
+            shortName = "t",
+            description = "the text you want to classify. If not specified, will read it from STDIN continuously."
+        )
+        val root by parser.option(ArgType.String, description = "path to root certificate chain, if using TLS")
+        val key by parser.option(ArgType.String, description = "path to secret key file, if using mutual TLS")
+        val cert by parser.option(ArgType.String, description = "path to public certificate, if using mutual TLS")
+        parser.parse(args)
 
-    val channel: GrpcChannelChoice = GrpcChannelChoice.fromArgs(root = root, cert = cert, key = key)
-    val clients = GLOBAL_CONFIG.classificationServices.map { service -> ClassificationClient(channel, service) }
+        val channel: GrpcChannelChoice = GrpcChannelChoice.fromArgs(root = root, cert = cert, key = key)
+        val clients = GLOBAL_CONFIG.classificationServices.map { service -> ClassificationClient(channel, service) }
 
-    if (text != null) {
-        clients.classify(text!!)
-    } else {
-        while (true) {
-            println("Enter text you want to classify, or CTRL-D to exit.")
-            val nextText = readLine() ?: break
+        if (text != null) {
+            clients.classify(text!!)
+        } else {
+            while (true) {
+                println("Enter text you want to classify, or CTRL-D to exit.")
+                val nextText = readLine() ?: break
 
-            clients.classify(nextText)
+                clients.classify(nextText)
+            }
         }
     }
 }
