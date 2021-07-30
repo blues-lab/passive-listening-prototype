d3e05d602cdad3390893b6775a3e540eff524e94
bhuvanbasireddy <bhvrddy@gmail.com>
2021-07-30 12:26:37 -0700

    Modifed pipeline to use permission poll for initial classification

--- classify_client/src/main/kotlin/plp/classify/ClassificationClient.kt
+++ classify_client/src/main/kotlin/plp/classify/ClassificationClient.kt
@@ -6,6 +6,7 @@ import plp.common.rpc.client.GrpcChannelChoice
 import plp.logging.KotlinLogging
 import plp.proto.Classification
 import plp.proto.ClassificationServiceGrpcKt
+import plp.proto.PermissionPoll
 
 private val logger = KotlinLogging.logger {}
 
@@ -21,7 +22,8 @@ open class ClassificationClient(grpcChannelChoice: GrpcChannelChoice, service: S
     suspend fun classifyText(text: String): Classification.ClassificationResponse {
         val timestamp = currentUnixTime()
         logger.debug { "$timestamp requesting classification of $text" }
-        val request = Classification.ClassificationRequest.newBuilder().setId(timestamp).setText(text).build()
+        // val request = Classification.ClassificationRequest.newBuilder().setId(timestamp).setText(text).build()
+        val request = PermissionPoll.PermissionPollRequest.newBuilder().setContent(text).build()
         val response = stub.classifyText(request)
         logger.info { "${currentUnixTime()} received response $response" }
         return response
